57b39c084f46584b274dbc00e7c72c279b7c9dd1
Mnat69 <nathan-maillard@orange.fr>
2016-02-29 19:58:09 +0100

    Popup + timer + retour accueil

--- Swift-Project/Swift-Project/ViewController.swift
+++ Swift-Project/Swift-Project/ViewController.swift
@@ -20,6 +20,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func Play(sender: AnyObject) {
+        NSTimer.scheduledTimerWithTimeInterval(5.0, target: self, selector: Selector("popupResult"), userInfo: nil, repeats: false)
+    }
+    
+    func popupResult() {
+        let alert = UIAlertController(title: "Perdu", message: "Temps écoulé", preferredStyle: .Alert)
+        
+        let firstAction = UIAlertAction(title: "Accueil", style: .Default) {
+            (action) in
+            if let navController = self.navigationController{
+                navController.popViewControllerAnimated(true)
+            }
+            
+        }
+        
+        alert.addAction(firstAction)
+        presentViewController(alert, animated: true, completion:nil)
+    }
 
 }
 
