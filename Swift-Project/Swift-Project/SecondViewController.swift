dc3c7ca2894384da0979c138145f190c71aed7e1
Mnat69 <nathan-maillard@orange.fr>
2016-02-29 17:52:13 +0100

    Navigation entre deux pages

---
+++ Swift-Project/Swift-Project/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  Swift-Project
+//
+//  Created by Nathan Maillard on 24/02/2016.
+//  Copyright © 2016 Nathan Maillard. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
